ebc982c348e3fb220a174e72fba5aa93470654d0
Anjia Wang <anjiawang@gmail.com>
2021-01-14 18:31:40 +0000

    Update the fixed omp target map example

--- REXCompiler/REXReferenceManual/target_map/rex_lib_target.cu
+++ REXCompiler/REXReferenceManual/target_map/rex_lib_target.cu
@@ -10,7 +10,7 @@ extern "C" {
 __device__ int32_t omptarget_device_environment;
 __device__ char OUT__1__8060__main__15__kernel___exec_mode = 0;
 
-__global__ void OUT__1__8060__main__15__kernel__(int b,int c,int d)
+__global__ void OUT__1__8060__main__15__kernel__(int *bp__,int *cp__,int *dp__)
 {
   int _p_i;
   int _dev_lower;
@@ -24,12 +24,12 @@ __global__ void OUT__1__8060__main__15__kernel__(int b,int c,int d)
   while(XOMP_static_sched_next(&_dev_loop_sched_index,2,1,_dev_loop_stride,_dev_loop_chunk_size,_dev_thread_num,_dev_thread_id,&_dev_lower,&_dev_upper))
     for (_p_i = _dev_lower; _p_i <= _dev_upper; _p_i += 1) {
 // 4, 0/random, 8
-      printf("Test1 %d, %d, %d.\n",b,c,d);
-      b = 40;
-      c = 60;
-      d = 80;
+      printf("Test1 %d, %d, %d.\n", *bp__, *cp__, *dp__);
+       *bp__ = 40;
+       *cp__ = 60;
+       *dp__ = 80;
 // 40, 60, 80
-      printf("Test2 %d, %d, %d.\n",b,c,d);
+      printf("Test2 %d, %d, %d.\n", *bp__, *cp__, *dp__);
     }
 }
 #ifdef __cplusplus
