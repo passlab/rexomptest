63b38180add40fef35c2becad51b94d944b66dc6
Anjia Wang <anjiawang@gmail.com>
2021-01-16 07:59:26 +0000

    Update the example of omp target parallel for

--- REXCompiler/REXReferenceManual/target_parallel_for/rose_target.c
+++ REXCompiler/REXReferenceManual/target_parallel_for/rose_target.c
@@ -1,9 +1,15 @@
 #include "rex_kmp.h" 
+struct __tgt_bin_desc *__cubin_desc = 0;
+
+void  __attribute__((destructor)) unregister_kernel_entries()
+{
+  __tgt_unregister_lib(__cubin_desc);
+}
 struct __tgt_offload_entry __offload_entries[2];
 struct __tgt_offload_entry *__start_omp_offloading_entries = &__offload_entries[0];
 struct __tgt_offload_entry *__stop_omp_offloading_entries = 0;
 
-void register_kernel_entries()
+void  __attribute__((constructor)) register_kernel_entries()
 {
   void *OUT__1__9009__main__13__entry_ptr__ = (void *)9009;
   struct __tgt_offload_entry OUT__1__9009__main__13__omp_offload_entry__ = {OUT__1__9009__main__13__entry_ptr__, "OUT__1__9009__main__13__kernel__", 0, 0, 0};
@@ -12,6 +18,8 @@ void register_kernel_entries()
   struct __tgt_offload_entry OUT__2__9009__main__7__omp_offload_entry__ = {OUT__2__9009__main__7__entry_ptr__, "OUT__2__9009__main__7__kernel__", 0, 0, 0};
   __offload_entries[1] = OUT__2__9009__main__7__omp_offload_entry__;
   __stop_omp_offloading_entries = &__offload_entries[2];
+  char cuda_entry_name[] = "rex_lib_target.cubin";
+  __cubin_desc = register_cubin(cuda_entry_name,__start_omp_offloading_entries,__stop_omp_offloading_entries);
 }
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,14 +29,10 @@ int main(int argc,char **argv)
   int status = 0;
   int i;
 {
-    if (__stop_omp_offloading_entries == 0) 
-      register_kernel_entries();
 /* Launch CUDA kernel ... */
     int _threads_per_block_ = 1024;
     int _num_blocks_ = 256;
     int64_t __device_id = 0;
-    char cuda_entry_name[] = "rex_lib_target.cubin";
-    struct __tgt_bin_desc *bin_desc = register_cubin(cuda_entry_name,__start_omp_offloading_entries,__stop_omp_offloading_entries);
     void *__host_ptr = (void *)9010;
     void *__args_base[] = {};
     void *__args[] = {};
@@ -36,17 +40,12 @@ int main(int argc,char **argv)
     int64_t __arg_types[] = {};
     int32_t __arg_num = 0;
     __tgt_target_teams(__device_id,__host_ptr,__arg_num,__args_base,__args,__arg_sizes,__arg_types,_threads_per_block_,_num_blocks_);
-    __tgt_unregister_lib(bin_desc);
   }
 {
-    if (__stop_omp_offloading_entries == 0) 
-      register_kernel_entries();
 /* Launch CUDA kernel ... */
     int _threads_per_block_ = 1024;
     int _num_blocks_ = 256;
     int64_t __device_id = 0;
-    char cuda_entry_name[] = "rex_lib_target.cubin";
-    struct __tgt_bin_desc *bin_desc = register_cubin(cuda_entry_name,__start_omp_offloading_entries,__stop_omp_offloading_entries);
     void *__host_ptr = (void *)9009;
     void *__args_base[] = {};
     void *__args[] = {};
@@ -54,7 +53,6 @@ int main(int argc,char **argv)
     int64_t __arg_types[] = {};
     int32_t __arg_num = 0;
     __tgt_target_teams(__device_id,__host_ptr,__arg_num,__args_base,__args,__arg_sizes,__arg_types,_threads_per_block_,_num_blocks_);
-    __tgt_unregister_lib(bin_desc);
   }
   return 0;
 }
