e9094ba59af9022038dd3e33b5882bbccaf89c87
Xinyao Yi <yxyict@gmail.com>
2021-08-27 01:46:17 +0000

    Fixed the errors in matvec

--- REXCompiler/omptests/rex-simd/simd_dev/simd_paper_test/matvec/matvec_float.c
+++ REXCompiler/omptests/rex-simd/simd_dev/simd_paper_test/matvec/matvec_float.c
@@ -1,13 +1,14 @@
-//matvec.c
-//Multiplies a matrix by a vector
+#include "rex_kmp.h" 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <sys/timeb.h>
 #include <malloc.h>
+#include <immintrin.h> 
 
-#define N_RUNS 1000
-#define N 1200
+#define N_RUNS 20
+#define N 10240
 
 // read timer in second
 double read_timer() {
@@ -17,44 +18,43 @@ double read_timer() {
 }
 
 //Create a matrix and a vector and fill with random numbers
-void init(float **matrix, float *vector) {
+void init(float *matrix, float *vector) {
     for (int i = 0; i<N; i++) {
         for (int j = 0; j<N; j++) {
-            matrix[i][j] = (float)rand()/(float)(RAND_MAX/10.0);
+            matrix[i*N+j] = (float)rand()/(float)(RAND_MAX/10.0);
         }
         
         vector[i] = (float)rand()/(float)(RAND_MAX/10.0);
     }
 }
 
-//Our sum function- what it does is pretty straight-forward.
-void sum(float **matrix, float *vector, float **dest) {
-    float s = 0;
-    
+void matvec_simd(float *matrix, float *vector, float *dest) {
     for (int i = 0; i<N; i++) {
-        s = 0;
-        
-        #pragma omp simd    
+        int tmp = 0;
+        #pragma omp simd  
         for (int j = 0; j<N; j++) {
-            dest[i][j] = matrix[i][j] * vector[j];
+            tmp += matrix[i*N+j] * vector[j];
         }
+        dest[i] = tmp;
     }
 }
 
 // Debug functions
-void serial(float **matrix, float *vector, float **dest) {
+void matvec_serial(float *matrix, float *vector, float *dest) {
     for (int i = 0; i<N; i++) {
+        int tmp = 0;
         for (int j = 0; j<N; j++) {
-            dest[i][j] = matrix[i][j] * vector[j];
+            tmp += matrix[i*N+j] * vector[j];
         }
+        dest[i] = tmp;
     }
 }
 
-void print_matrix(float **matrix) {
+void print_matrix(float *matrix) {
     for (int i = 0; i<8; i++) {
         printf("[");
         for (int j = 0; j<8; j++) {
-            printf("%.2f ", matrix[i][j]);
+            printf("%.2f ", matrix[i*N+j]);
         }
         puts("]");
     }
@@ -69,47 +69,42 @@ void print_vector(float *vector) {
     puts("]");
 }
 
-float check(float **A, float **B){
+float check(float *A, float *B){
     float difference = 0;
     for(int i = 0;i<N; i++){
-        for (int j = 0; j<N; j++)
-        { difference += A[i][j]- B[i][j];}
+        difference += A[i]- B[i];
     }
     return difference;
 }
 
 int main(int argc, char **argv) {
     //Set everything up
-    float **dest_matrix = malloc(sizeof(float*)*N);
-    float **serial_matrix = malloc(sizeof(float*)*N);
-    float **matrix = malloc(sizeof(float*)*N);
+    float *dest_vector = malloc(sizeof(float*)*N);
+    float *serial_vector = malloc(sizeof(float*)*N);
+    float *matrix = malloc(sizeof(float*)*N*N);
     float *vector = malloc(sizeof(float)*N);
-    
-    for (int i = 0; i<N; i++) {
-        dest_matrix[i] = malloc(sizeof(float)*N);
-        serial_matrix[i] = malloc(sizeof(float)*N);
-        matrix[i] = malloc(sizeof(float)*N);
-    }
     
     srand(time(NULL));
     init(matrix, vector);
     
+    double t = 0;
     double start = read_timer();
     for (int i = 0; i<N_RUNS; i++)
-        sum(matrix, vector, dest_matrix);
-    double t = (read_timer() - start);
+        matvec_simd(matrix, vector, dest_vector);
+    t += (read_timer() - start);
     
+    double t_serial = 0;
     double start_serial = read_timer();
     for (int i = 0; i<N_RUNS; i++)
-        serial(matrix, vector, serial_matrix);
-    double t_serial = (read_timer() - start_serial);
+        matvec_serial(matrix, vector, serial_vector);
+    t_serial += (read_timer() - start_serial);
     
     print_matrix(matrix);
     print_vector(vector);
     puts("=\n");
-    print_matrix(dest_matrix);
+    print_vector(dest_vector);
     puts("---------------------------------");
-    print_matrix(serial_matrix);
+    print_vector(serial_vector);
     
     double gflops = ((2.0 * N) * N * N_RUNS) / (1.0e9 * t);
     double gflops_serial = ((2.0 * N) * N * N_RUNS) / (1.0e9 * t_serial);
@@ -117,12 +112,12 @@ int main(int argc, char **argv) {
     printf("==================================================================\n");
     printf("Performance:\t\t\tRuntime (s)\t GFLOPS\n");
     printf("------------------------------------------------------------------\n");
-    printf("Matrix-vector (SIMD):\t\t%4f\t%4f\n", t, gflops);
-    printf("Matrix-vector (Serial):\t\t%4f\t%4f\n", t_serial, gflops_serial);
-    printf("Correctness check: %f\n", check(dest_matrix,serial_matrix));
+    printf("Matrix-vector (SIMD):\t\t%4f\t%4f\n", t/N_RUNS, gflops);
+    printf("Matrix-vector (Serial):\t\t%4f\t%4f\n", t_serial/N_RUNS, gflops_serial);
+    printf("Correctness check: %f\n", check(dest_vector,serial_vector));
     
-    free(dest_matrix);
-    free(serial_matrix);
+    free(dest_vector);
+    free(serial_vector);
     free(matrix);
     free(vector);
     
