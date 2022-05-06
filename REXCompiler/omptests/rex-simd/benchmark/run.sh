7e17d6966be501a3bf432b13b4e2b21d79f68444
patrickf2000 <dev@patrickflynn.co>
2022-05-06 17:39:52 +0000

    Updated Intel CSV

--- REXCompiler/omptests/rex-simd/benchmark/run.sh
+++ REXCompiler/omptests/rex-simd/benchmark/run.sh
@@ -75,7 +75,7 @@ function run_intel() {
         run_program $1 "1_pf" $CSV
         run_program $1 "2_pf" $CSV
         run_program $1 "_rex" $CSV
-        run_program $1 "_rex_unroll" $CSV
+        run_program $1 "_unroll_rex" $CSV
         run_program $1 "_rex_p" $CSV
 	    run_program $1 "_rex_pf" $CSV
 
